63f973843f41c18b0f8979b381ece61a6f6fac78
Daniel <cyubahirodaniel@gmail.com>
2025-04-07 08:57:47 +0200

    Cleanup and using ES modules

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import 'dotenv/config';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(
+    path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+export default app;
