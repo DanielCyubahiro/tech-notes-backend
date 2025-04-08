05fe1cd5cbcd47aa27ea1981715894367e463532
Daniel <cyubahirodaniel@gmail.com>
2025-04-08 15:32:15 +0200

    Update notes CRUD

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -2,24 +2,41 @@ import Note from '../models/note.model.js';
 import ApiResponse from '../utils/response.util.js';
 import ApiError from '../utils/error.util.js';
 
+/**
+ * @param {*} req
+ * @param {*} res
+ * @param {NextFunction|Response<*, Record<string, *>>} next
+ * @desc Get all notes
+ * @route GET /notes
+ * @access Private
+ */
 const getAllNotes = async (req, res, next) => {
   try {
-    const notes = await Note.find();
-    return res.status(200).json(new ApiResponse(
+    const notes = await Note.find().lean();
+
+    return res.json(new ApiResponse(
         200,
         notes,
-        'Notes retrieved successfully',
+        notes.length ? 'Notes retrieved successfully' : 'No notes found',
     ));
   } catch (error) {
     next(error);
   }
 };
 
+/**
+ * @param {*} req
+ * @param {*} res
+ * @param {NextFunction|Response<*, Record<string, *>>} next
+ * @desc Get a note by id
+ * @route GET /notes/:id
+ * @access Private
+ */
 const getNote = async (req, res, next) => {
   try {
     const note = await Note.findById(req.params.id);
     if (!note) return next(new ApiError(404, 'Note not found'));
-    return res.status(200).json(new ApiResponse(
+    return res.json(new ApiResponse(
         200,
         note,
         'Note retrieved successfully',
@@ -29,10 +46,18 @@ const getNote = async (req, res, next) => {
   }
 };
 
+/**
+ * @param {*} req
+ * @param {*} res
+ * @param {NextFunction|Response<*, Record<string, *>>} next
+ * @desc Create a new note
+ * @route POST /notes
+ * @access Private
+ */
 const createNote = async (req, res, next) => {
   try {
     const newNote = await Note.create(req.body);
-    return res.status(201).json(new ApiResponse(
+    return res.json(new ApiResponse(
         201,
         newNote,
         'Note created successfully'));
@@ -43,13 +68,13 @@ const createNote = async (req, res, next) => {
 
 const updateNote = async (req, res, next) => {
   try {
-    let note = await Note.findById(req.params.id);
+    let note = await Note.findById(req.params.id).lean();
     if (!note) return next(new ApiError(404, 'Note not found'));
     note = await Note.findByIdAndUpdate(req.params.id, req.body, {
       new: true, // Return the updated document
     });
 
-    return res.status(200).json(new ApiResponse(
+    return res.json(new ApiResponse(
         200,
         note,
         'Note updated successfully',
@@ -62,7 +87,7 @@ const updateNote = async (req, res, next) => {
 const deleteNote = async (req, res, next) => {
   let note = await Note.findByIdAndDelete(req.params.id);
   if (!note) return next(new ApiError(404, 'Note not found'));
-  return res.status(200).json(new ApiResponse(
+  return res.json(new ApiResponse(
       200,
       note,
       'Note deleted successfully',
